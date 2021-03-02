0ff65c8f07aed35086ce9c85578fcf5f98b8aca1
Hmc1011 <Hmc10111994@gmail.com>
2021-03-02 16:12:26 +0700

    modified to singleton pattern

--- Router/Route.php
+++ Router/Route.php
@@ -1,5 +1,25 @@
 <?php
 class Route {
+    private static $instances = [];
+    protected function __construct()
+    {
+
+    }
+    protected function __clone() { }
+    public function __wakeup()
+    {
+        throw new \Exception("Cannot unserialize a singleton.");
+    }
+    public static function getInstance(): Route
+    {
+        $cls = static::class;
+        if (!isset(self::$instances[$cls])) {
+            self::$instances[$cls] = new static();
+        }
+
+        return self::$instances[$cls];
+    }
+
     public static function routeToController(){
        $uri= $_SERVER['REQUEST_URI'];
        $path= parse_url($uri,PHP_URL_PATH);
